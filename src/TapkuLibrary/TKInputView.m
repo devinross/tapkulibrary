bf63f8a62b1b2992003225c5f42f00f5de142817
Devin Ross <devinross14@mac.com>
2014-03-21 15:35:14 -0400

    Adjust the textField:shouldChangeCharactersInRange:replacementString: for backspace key.

--- src/TapkuLibrary/TKInputView.m
+++ src/TapkuLibrary/TKInputView.m
@@ -193,7 +193,7 @@
 		BOOL delete = YES;
 		
 		if(self.textField.delegate && [self.textField.delegate respondsToSelector:@selector(textField:shouldChangeCharactersInRange:replacementString:)])
-			delete = [self.textField.delegate textField:self.textField shouldChangeCharactersInRange:NSMakeRange(self.textField.text.length-1, 0) replacementString:@""];
+			delete = [self.textField.delegate textField:self.textField shouldChangeCharactersInRange:NSMakeRange(self.textField.text.length-1, 1) replacementString:@""];
 		
 		if(delete)
 			[self.textField deleteBackward];
